77c845de34d320357ef309b364d3f040241a4c84
Omer <omerm2010@ofekschool.org.il>
2026-04-01 23:30:23 +0300

    Add files via upload

---
+++ be/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../fe")));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+app.get("/get", (req, res) => {
+    console.log("GET request data:", req.query);
+    res.send("GET request התקבל!");
+});
+
+app.post("/submit", (req, res) => {
+    console.log("POST request data:", req.body);
+    res.send("Form data received");
+});
+
+
+app.listen(port, () => {
+    console.log(`השרת רץ: http://localhost:${port}`);
+});
